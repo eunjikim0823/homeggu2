a08adda0dbd8da82cb5aaf6e2f47a699fda9c957
eunjikim <apdlf0708@naver.com>
2021-05-23 23:13:15 +0900

    memberdto 편집

---
+++ src/main/java/Member/MembersDaoImpl.java
@@ -0,0 +1,53 @@
+package Member;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.members.domain.AdminDTO;
+import com.members.domain.MembersDTO;
+import com.reservation.domain.ReservationDTO;
+
+@Repository
+public class MembersDaoImpl implements MembersDAO {
+
+	@Autowired
+	public SqlSession sqlSession;
+	
+	@Override
+	public int idCheck(MembersDTO members) {
+		return sqlSession.selectOne("idCheck",members);
+		}
+	
+	@Override
+	public void userJoin(MembersDTO members) {
+		sqlSession.insert("userJoin",members);
+	}
+
+	@Override
+	public MembersDTO getOne(MembersDTO members) {
+		return sqlSession.selectOne("getOne",members);
+	}
+	
+	@Override
+	public MembersDTO getMember(String member_id) {
+	     return sqlSession.selectOne("getMember",member_id);
+	 }
+	
+
+	@Override
+  	public void updateMember(MembersDTO members) {
+	     sqlSession.update("updateMember", members);
+	}
+	   
+	@Override
+	public void deleteMember(MembersDTO members) {
+	   sqlSession.delete("deleteMember", members);
+	}
+	   
+	@Override
+	public AdminDTO adminGetOne(AdminDTO admins) {
+		return sqlSession.selectOne("adminGetOne",admins);
+		}
+
+}
